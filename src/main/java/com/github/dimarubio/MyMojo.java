b0f041550f7a631bd8dfb9804f4f45f4ef9a2c5c
d.bolyachin <pr0st0123456>
2018-12-04 18:56:10 +0200

    added additional configuration property "outputDirectory"

--- src/main/java/com/github/dimarubio/MyMojo.java
+++ src/main/java/com/github/dimarubio/MyMojo.java
@@ -8,6 +8,10 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
 
 /**
  * Goal and phase.
@@ -16,7 +20,7 @@ import java.io.File;
 public class MyMojo
     extends AbstractMojo
 {
-    @Parameter( defaultValue = "${project.build.directory}", property = "getTestCasesIDFromRun.outputDir", required = true )
+    @Parameter( property = "outputDirectory", required = true )
     private File outputDirectory;
 
     @Parameter( property = "testRailURL", required = true )
@@ -36,6 +40,9 @@ public class MyMojo
     {
         getLog().info("Start preparing property file with testcases ID");
         getLog().info("Testrail run number: " + numberOfTestRailRun);
+        if (!outputDirectory.exists()) {
+            outputDirectory.mkdirs();
+        }
         try
         {
             TestRailHelper testRailHelper = new TestRailHelper( testRailURL, userEmail, password, numberOfTestRailRun);
@@ -43,13 +50,34 @@ public class MyMojo
             for (Long id: testRailHelper.getTestCasesIDFromRun()) {
                 testCasesTags = String.format("%s@C%s,", testCasesTags, id);
             }
-            new PropertiesHelper().saveProperties("testCasesTags", testCasesTags);
-            getLog().info("File testCaseIDFromRUN.properties is generated");
+            saveProperties("testCasesTags", testCasesTags);
+            getLog().info("File testCaseIDFromRUN.properties is generated to " + outputDirectory.toString());
         }
         catch ( Exception e )
         {
             getLog().error(e.fillInStackTrace());
             throw new MojoExecutionException( "Error ", e );
+        }
+    }
+
+    public void saveProperties(String tagName, String tagValue) throws IOException {
+        Properties prop = new Properties();
+        OutputStream output = null;
+        try {
+            File propertyFile = new File( outputDirectory,"testCaseIDFromRUN.properties");
+            output = new FileOutputStream(propertyFile);
+            prop.setProperty(tagName, tagValue);
+            prop.store(output, null);
+        } catch (IOException io) {
+            io.printStackTrace();
+        } finally {
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
